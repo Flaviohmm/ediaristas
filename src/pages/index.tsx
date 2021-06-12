40aa1510badd5dff0f42a8f674c083c5786e868f
Flaviohmm <fhenrique609@gmail.com>
2021-06-11 23:36:51 -0300

    FormElementsContainer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironm
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
+import { Button, Typography } from "@material-ui/core";
+import { FormElementsContainer } from "ui/styles/pages/index.style";
 
 export default function Home() {
   return (
@@ -14,12 +16,24 @@ export default function Home() {
         }
       />
 
-      <TextFieldMask
-        mask={"99.999-999"}
-        label={"Digite seu CEP"}
-        fullWidth
-        variant={"outlined"}
-      />
+      <FormElementsContainer>
+        <TextFieldMask
+          mask={"99.999-999"}
+          label={"Digite seu CEP"}
+          fullWidth
+          variant={"outlined"}
+        />
+
+        <Typography color={"error"}>CEP Inválido</Typography>
+
+        <Button
+          variant={"contained"}
+          color={"secondary"}
+          sx={{ width: "220px" }}
+        >
+          Buscar
+        </Button>
+      </FormElementsContainer>
 
       <UserInformation
         name={"Flavio Mendes"}
